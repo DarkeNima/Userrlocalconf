d2fe981414adb66cda32dea4166791207f9f613f
DarkeNima <naviyaoficial@gmail.com>
2026-04-23 21:51:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     
-    const responseData = {
-      "code": 0,
-      "message": "Success",
-      // මෙන්න මේ MD5 ටික අනිවාර්යයෙන්ම ඕනේ OB53 වලට
-      "md5": "7e94677df24a33519a49c4cfc85edf41",
-      "pkg_md5": "99f1b4b2b23b52e79d16c93a851d1b35",
-      "is_server_open": true,
-      "latest_release_version": "1.103.1", 
-      "current_release_version": "1.103.1",
-      "remote_version": "1.103.1",
-      "server_url": "http://139.162.54.41:10001", 
-      "cdn_url": "http://client.common.freefiremobile.com", // Official CDN එක දීම වඩාත් ආරක්ෂිතයි
-      "client_ip": req.connection.remoteAddress, // මේකෙන් user ගේ ඇත්තම IP එක ගන්නවා
-      "ggp_url": "139.162.54.41:10001",
-      "country_code": "IN",
-      "is_firewall_open": false,
-      "maintenance": false
-    };
+    
+const responseData = {
+  "code": 2, // 0 වෙනුවට 2 දාලා බලමු
+  "is_server_open": true,
+  "latest_release_version": "1.103.1", 
+  "remote_version": "1.103.1",
+  "server_url": "http://139.162.54.41:10001",
+  "ggp_url": "139.162.54.41:10001",
+  "country_code": "IN",
+  "cdn_url": "http://client.common.freefiremobile.com/",
+  "gamevar": "var_name,comment,var_type,var_value\nANOEmulatorCheckDisbaledClientVariant,bypass,string,\"ClientUsingVersion_FFI,ClientUsingVersion_MAX,ClientUsingVersion_NORMAL\"\n",
+  "is_firewall_open": false,
+  "garena_hint": false,
+  "garena_login": false
+};
 
     res.end(JSON.stringify(responseData));
 });
