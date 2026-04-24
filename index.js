0743952093c525cb092026334cad5adf507df407
DarkeNima <naviyaoficial@gmail.com>
2026-04-24 08:59:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // ගේම් එකෙන් එන හැම request එකකටම JSON response එකක් යවනවා
+    // 📝 මෙන්න මෙතනින් තමයි ගේම් එක අපෙන් ඉල්ලන දේ ලොග් වෙන්නේ
+    console.log(`\n[${new Date().toLocaleString()}] --- Incoming Request ---`);
+    console.log(`Method: ${req.method}`);
+    console.log(`URL: ${req.url}`);
+    console.log(`Headers:`, req.headers);
+
     res.writeHead(200, { 
         'Content-Type': 'application/json',
         'Connection': 'close' 
     });
     
     const responseData = {
-        "code": 2, // OB53 වල ලොගින් එකට පනින්න මේක '2' වෙන්නම ඕනේ
+        "code": 2,
         "is_server_open": true,
         "latest_release_version": "1.103.1", 
         "remote_version": "1.103.1",
         "server_url": "http://139.162.54.41:10001",
-        "ggp_url": "139.162.54.41:10001", // TCP කනෙක්ෂන් එකට මේක වැදගත්
+        "ggp_url": "139.162.54.41:10001",
         "country_code": "IN",
         "cdn_url": "http://client.common.freefiremobile.com/",
         "gamevar": "var_name,comment,var_type,var_value\nANOEmulatorCheckDisbaledClientVariant,bypass,string,\"ClientUsingVersion_FFI,ClientUsingVersion_MAX,ClientUsingVersion_NORMAL\"\n",
         "is_firewall_open": false,
         "garena_hint": false,
         "garena_login": false,
-        "client_ip": "139.162.54.41" // ඔයාගේ VPS IP එක මෙතනටත් දැම්මා
+        "client_ip": "139.162.54.41"
     };
 
-    console.log(`[${new Date().toLocaleString()}] Version Check Request from Client`);
     res.end(JSON.stringify(responseData));
 });
 
 const PORT = 80;
-
-// වැදගත්: VPS එකේ Port 80 රන් කරන්න නම් 'sudo' පාවිච්චි කරන්න වෙනවා
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Version API (Port 80) is LIVE!`);
-    console.log(`🚀 Redirecting Game to TCP Port 10001`);
+    console.log(`👀 Version API Debugger is watching on Port ${PORT}`);
 });
