eb6f12d89db117badc52d53b18038c2c42ed7f0c
DarkeNima <naviyaoficial@gmail.com>
2026-05-24 08:55:01 +0530

    Update ver.js

--- ver.js
+++ ver.js
@@ -1,11 +1,9 @@
-// ver.js
-const MY_IP = '103.6.168.170';
-const MY_DOMAIN = 'navivpn.sytes.net';
-const MY_URL_HTTPS = `https://${MY_DOMAIN}`;
+const express = require('express');
+const router = express.Router();
+const config = require('./config');
 
-function handleVerReq(req, res) {
+router.get('/ver.php', (req, res) => {
     const clientIp = req.ip.replace('::ffff:', '');
-    
     const verData = {
         "code": 0,
         "is_server_open": true,
@@ -23,23 +21,22 @@ function handleVerReq(req, res) {
         "should_check_ab_load": false,
         "force_refresh_restype": "optionalavatarres",
         "remote_version": "1.123.8",
-        "server_url": `${MY_URL_HTTPS}/`, 
+        "server_url": `${config.MY_URL_HTTPS}/`, 
         "is_review_server": false,
         "use_login_optional_download": true,
-        "use_background_download": false,
-        "use_background_download_lobby": false,
+        "use_background_download": true,
+        "use_background_download_lobby": true,
         "country_code": "SG",
         "client_ip": clientIp,
         "gdpr_version": 0,
         "billboard_cdn_url": "https://dl-tata.freefireind.in/common/OB53/CSH/patchupdate/indhfuHFHf101.ff_extend",
-        "ggp_url": MY_IP,
-        "core_url": MY_IP,
-        "core_ip_list": [MY_IP, "0.0.0.0"]
+        "ggp_url": config.MY_IP,
+        "core_url": config.MY_IP,
+        "core_ip_list": [config.MY_IP, "0.0.0.0"]
     };
-
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(verData);
-    console.log(`✅ ver.php data sent with all fields`);
-}
+    console.log(`✅ ver.php sent`);
+});
 
-module.exports = { handleVerReq };
+module.exports = router;
