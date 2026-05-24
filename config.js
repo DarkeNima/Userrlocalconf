e18aff12f4b0dbe1f2e6e1bcd4fe22fe1f1ce066
DarkeNima <naviyaoficial@gmail.com>
2026-05-24 08:54:28 +0530

    Update and rename routes.js to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HTTP_PORT: 80,
+    HTTPS_PORT: 443,
+    TCP_PORT: 7006,
+    MY_DOMAIN: 'naviiautsrv.myftp.org',
+    MY_IP: '129.150.38.255',
+    MY_URL_HTTPS: 'https://naviiautsrv.myftp.org',
+    TARGET_HOST: 'loginbp.ggpolarbear.com'
+};
