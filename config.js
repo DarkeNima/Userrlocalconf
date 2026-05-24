1f161c81c74e247b7e4054a9529a80919b3abee4
DarkeNima <naviyaoficial@gmail.com>
2026-05-24 16:16:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ module.exports = {
     MY_DOMAIN: 'naviiautsrv.myftp.org',
     MY_IP: '129.150.38.255',
     MY_URL_HTTPS: 'https://naviiautsrv.myftp.org',
-    TARGET_HOST: 'versions.garenanow.live/live/'
+    // ❌ කලින් තිබුණ වැරදි ක්‍රමය:
+// TARGET_HOST: 'versions.garenanow.live/live/'
+
+//  නිවැරදි ක්‍රමය (Domain එක විතරක් තියෙන්න ඕනේ):
+TARGET_HOST: 'versions.garenanow.live'
+
 };
